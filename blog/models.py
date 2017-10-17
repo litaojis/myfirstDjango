6ee03d51e37530be6fef3dca33179c0c6a0afd79
ajoker <601807116@qq.com>
2017-10-17 09:21:48 +0800

    merge

--- blog/models.py
+++ blog/models.py
@@ -1,23 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2016-11-26 18:54:49
+# @Author  : Weizhong Tu (mail@tuweizhong.com)
+# @Link    : http://www.tuweizhong.com
+# @Version : 0.0.1
+from __future__ import unicode_literals
+
 from django.db import models
-from django.contrib import admin
+from django.utils.encoding import python_2_unicode_compatible
 
 # Create your models here.
-class Article(models.Model):
-    title = models.CharField(u'标题', max_length=256)
-    content = models.TextField(u'内容')
- 
-    pub_date = models.DateTimeField(u'发表时间', auto_now_add=True, editable = True)
-    update_time = models.DateTimeField(u'更新时间',auto_now=True, null=True)
+@python_2_unicode_compatible
+class Author(models.Model):
+    name = models.CharField(max_length = 50)
+    qq = models.CharField(max_length = 10)
+    addr = models.TextField()
+    email = models.EmailField()
 
-    def __str__(self):# 在Python3中用 __str__ 代替 __unicode__
+    def __str__(self):
+        return self.name
+
+@python_2_unicode_compatible
+class Article(models.Model):
+    title = models.CharField(max_length = 100)
+    author = models.ForeignKey(Author)
+    content = models.TextField()
+    score = models.IntegerField()
+    tags = models.ManyToManyField('Tag')
+
+    def __str__(self):
         return self.title
 
-class Person(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
- 
-    def my_property(self):
-        return self.first_name + ' ' + self.last_name
-    my_property.short_description = "Full name of the person"
- 
-    full_name = property(my_property)
+class Tag(models.Model):
+    name = models.CharField(max_length = 50)
+
+    def __str__(self):
+        return self.name
+    
---
+++ initdb.py
@@ -0,0 +1,48 @@
+# coding:utf-8
+
+from __future__ import unicode_literals
+import random
+from myfirstDjango.wsgi import *
+from blog.models import Author,Article,Tag
+
+author_name_list = ['Ltao','Ajoker','Lushan','YTXX']
+article_title_list = ['Django教程','Git入门','Java从入门到放弃','Vue&Python快速构建web应用']
+
+
+def createAuthors():
+    for author_name in author_name_list:
+        author,created = Author.objects.get_or_create(name = author_name)
+        author.qq = ''.join( 
+            str(random.choice(range(10))) for _ in range(9)   
+        )
+        author.addr = 'addr_%s' %(random.randrange(1,3))
+        author.email = '%s@qq.com' %(author.addr)
+        author.save()
+
+def createArticleAndTag():
+    #随机生成文章
+    for article_title in article_title_list:
+        tagname = article_title.split(' ',1)[0]
+        tag,created = Tag.objects.get_or_create(name = tagname)
+
+        random_author = random.choice(Author.objects.all())
+
+        for i in range(1,21):
+            title = '%s_%s' % (article_title,i)
+            article,created = Article.objects.get_or_create(
+                title = title,defaults = {
+                    'author':random_author,
+                    'content':'%s 正文' % title,
+                    'score': random.randrange(70,101),
+                }
+            )
+            article.tags.add(tag)
+
+
+def main():
+    createAuthors()
+    createArticleAndTag()
+
+if __name__ == '__main__':
+    main()
+    print('Done Success!')
